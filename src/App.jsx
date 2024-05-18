123dc52126d02a7f9ca56ca76efa02d14b5c24a6
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2024-05-18 12:02:29 +0200

    Add: Variables, vercel.json file and router configured.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import style from "./App.module.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
-    return <div className={style.app}></div>;
+    return (
+        <Router>
+            <div className={style.app}>
+                <Routes></Routes>
+            </div>
+        </Router>
+    );
 }
