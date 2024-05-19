5c5c623074cfb9d54ec7a81fd413d3fa268ab544
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2024-05-19 15:10:40 +0200

    Add: Carousel added in the Bungalow component.

--- src/sections/Bungalow/Bungalow.jsx
+++ src/sections/Bungalow/Bungalow.jsx
@@ -1,11 +1,121 @@
 import style from "./Bungalow.module.css";
 import { useTranslation } from "react-i18next";
+import { useEffect, useRef, useState } from "react";
 import bungalowOne from "../../assets/images/bungalow-1.jpg";
 import BungalowTwo from "../../assets/images/bungalow-2.jpg";
 import BungalowThree from "../../assets/images/bungalow-3.jpg";
+import bungalowFour from "../../assets/images/bungalow-4.jpg";
+import BungalowFive from "../../assets/images/bungalow-5.jpg";
+import BungalowSix from "../../assets/images/bungalow-6.jpg";
+import { IoArrowBackCircleSharp, IoArrowForwardCircleSharp } from "react-icons/io5";
 
 export default function Bungalow() {
     const [t, i18n] = useTranslation("global");
+    const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+    const [carouselIndex, setCarouselIndex] = useState(1);
+    const images = Array.from(document.querySelectorAll(".bungalowsCarouselImage"));
+
+    const onNext = () => {
+        switch (carouselIndex) {
+            case 1:
+                images.map((e) => {
+                    e.style.transform = "translateX(-100%)";
+                });
+                setCarouselIndex(2);
+                break;
+            case 2:
+                images.map((e) => {
+                    e.style.transform = "translateX(-200%)";
+                });
+                setCarouselIndex(3);
+                break;
+
+            case 3:
+                images.map((e) => {
+                    e.style.transform = "translateX(-300%)";
+                });
+                setCarouselIndex(4);
+                break;
+
+            case 4:
+                images.map((e) => {
+                    e.style.transform = "translateX(-400%)";
+                });
+                setCarouselIndex(5);
+                break;
+
+            case 5:
+                images.map((e) => {
+                    e.style.transform = "translateX(-500%)";
+                });
+                setCarouselIndex(6);
+                break;
+
+            case 6:
+                images.map((e) => {
+                    e.style.transform = "translateX(0)";
+                });
+                setCarouselIndex(1);
+                break;
+
+            default:
+                break;
+        }
+    };
+
+    const onPrev = () => {
+        switch (carouselIndex) {
+            case 1:
+                images.map((e) => {
+                    e.style.transform = "translateX(-500%)";
+                });
+                setCarouselIndex(6);
+                break;
+            case 2:
+                images.map((e) => {
+                    e.style.transform = "translateX(0)";
+                });
+                setCarouselIndex(1);
+                break;
+
+            case 3:
+                images.map((e) => {
+                    e.style.transform = "translateX(-100%)";
+                });
+                setCarouselIndex(2);
+                break;
+
+            case 4:
+                images.map((e) => {
+                    e.style.transform = "translateX(-200%)";
+                });
+                setCarouselIndex(3);
+                break;
+
+            case 5:
+                images.map((e) => {
+                    e.style.transform = "translateX(-300%)";
+                });
+                setCarouselIndex(4);
+                break;
+
+            case 6:
+                images.map((e) => {
+                    e.style.transform = "translateX(-400%)";
+                });
+                setCarouselIndex(5);
+                break;
+
+            default:
+                break;
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            setInnerWidth(window.innerWidth);
+        });
+    }, []);
 
     return (
         <section className={style.container}>
@@ -13,7 +123,6 @@ export default function Bungalow() {
                 <p className={style.titlesTitle}>{t("bungalow.title")}</p>
                 <div className={style.titlesSubtitle}>{t("bungalow.subtitle")}</div>
             </div>
-
             <div className={style.data}>
                 <ul className={style.dataList}>
                     <li>{t("bungalow.listOne")}</li>
@@ -32,17 +141,55 @@ export default function Bungalow() {
                     <li>{t("bungalow.listTwelve")}</li>
                 </ul>
             </div>
-            <div className={style.images}>
-                <div className={style.imagesImage}>
-                    <img src={bungalowOne} />
+            {innerWidth > 767 ? (
+                <div className={style.images}>
+                    <div className={style.imagesImage}>
+                        <img src={bungalowOne} />
+                    </div>
+                    <div className={style.imagesImage}>
+                        <img src={BungalowTwo} />
+                    </div>
+                    <div className={style.imagesImage}>
+                        <img src={BungalowThree} />
+                    </div>
+                    <div className={style.imagesImage}>
+                        <img src={bungalowFour} />
+                    </div>
+                    <div className={style.imagesImage}>
+                        <img src={BungalowFive} />
+                    </div>
+                    <div className={style.imagesImage}>
+                        <img src={BungalowSix} />
+                    </div>
                 </div>
-                <div className={style.imagesImage}>
-                    <img src={BungalowTwo} />
+            ) : (
+                <div className={style.imagesCarousel}>
+                    <div className={style.imagesCarouselBig}>
+                        <div className={`${style.imagesCarouselBigImage} bungalowsCarouselImage`}>
+                            <img src={bungalowOne} />
+                        </div>
+                        <div className={`${style.imagesCarouselBigImage} bungalowsCarouselImage`}>
+                            <img src={BungalowTwo} />
+                        </div>
+                        <div className={`${style.imagesCarouselBigImage} bungalowsCarouselImage`}>
+                            <img src={BungalowThree} />
+                        </div>
+                        <div className={`${style.imagesCarouselBigImage} bungalowsCarouselImage`}>
+                            <img src={bungalowFour} />
+                        </div>
+                        <div className={`${style.imagesCarouselBigImage} bungalowsCarouselImage`}>
+                            <img src={BungalowFive} />
+                        </div>
+                        <div className={`${style.imagesCarouselBigImage} bungalowsCarouselImage`}>
+                            <img src={BungalowSix} />
+                        </div>
+                    </div>
+                    <div className={style.imagesCarouselNavbar}>
+                        <IoArrowBackCircleSharp onClick={onPrev} />
+                        <IoArrowForwardCircleSharp onClick={onNext} />
+                    </div>
                 </div>
-                <div className={style.imagesImage}>
-                    <img src={BungalowThree} />
-                </div>
-            </div>
+            )}
             <p className={style.text}>{t("bungalow.text")}</p>
         </section>
     );
