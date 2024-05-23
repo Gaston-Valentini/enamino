a221484eb7a0f78cffd936749f440b93ee16e44e
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2024-05-23 19:08:32 +0200

    Add: Header section added.

---
+++ src/sections/Header/Header.jsx
@@ -0,0 +1,18 @@
+import style from "./Header.module.css";
+import { useTranslation } from "react-i18next";
+import logo from "../../assets/images/logo.png";
+
+export default function Header() {
+    const [t, i18n] = useTranslation("global");
+
+    return (
+        <section className={style.container}>
+            <div className={style.overlay}>
+                <div className={style.logo}>
+                    <img src={logo} />
+                </div>
+                <p className={style.slogan}>{t("header.slogan")}</p>
+            </div>
+        </section>
+    );
+}
--- src/views/Village/Village.jsx
+++ src/views/Village/Village.jsx
@@ -1,4 +1,5 @@
 import style from "./Village.module.css";
+import Header from "../../sections/Header/Header";
 import Camp from "../../sections/Camp/Camp";
 import Bungalow from "../../sections/Bungalow/Bungalow";
 import Menu from "../../sections/Menu/Menu";
@@ -7,6 +8,7 @@ import Enviroment from "../../sections/Enviroment/Enviroment";
 export default function Village() {
     return (
         <section className={style.container}>
+            <Header />
             <Camp />
             <Bungalow />
             <Menu />
